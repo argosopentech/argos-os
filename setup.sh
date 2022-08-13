4f9325f6b8b2992e57f7b8659b7767a88c41268b
P.J. Finlay <pjappdevelopment@gmail.com>
2022-08-13 12:21:55 -0500

    Install abricotine

--- setup.sh
+++ setup.sh
@@ -10,3 +10,10 @@ sudo cp argos-os-profile.sh /etc/profile.d/
 sudo rm -rf /usr/share/backgrounds/*
 
 sudo cp -r root/* /
+
+# Install Abricotine
+if [ $(sudo dpkg-query -l | grep abricotine | wc -l) -lt 1 ]
+then
+	wget -O run/abricotine_1.1.4_amd64.deb https://github.com/brrd/abricotine/releases/download/v1.1.4/abricotine_1.1.4_amd64.deb
+	sudo dpkg -i run/abricotine_1.1.4_amd64.deb
+fi
